9af38b7f1e92943c7bc13a601805f23bc2c2ecd0
Anna Shchiptsova <shchipts@iiasa.ac.at>
2021-09-06 13:36:48 +0200

    Init repo

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject org.clojars.shchipts/generic-dice "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "https://github.com/shchipts/generic-dice"
+  :scm {:name "git"
+        :url "https://github.com/shchipts/generic-dice"}
+  :license {:name "The MIT License"
+            :url "http://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [org.iiasa/utilities-clj "1.1.0"]]
+  :main nil)
